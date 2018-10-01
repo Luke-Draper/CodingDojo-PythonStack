f491e4f15a3d56469fd924184a7ca89cf7842905
Luke Draper <lukepdraper@gmail.com>
2018-10-01 12:44:40 -0500

    Completed Loop Basics

--- 3.Python/2.Fundamentals/1.For_Loop_Basic_1/assignment.py
+++ 3.Python/2.Fundamentals/1.For_Loop_Basic_1/assignment.py
@@ -1,37 +1,57 @@
-# Part 1
+print("Part 1")
 
-# Basic
+print("Basic")
 # Print all the numbers/integers from 0 to 150.
 
+for i in range(151):
+	print(i)
 
-
-# Multiples of Five
+print("Multiples of Five")
 # Print all the multiples of 5 from 5 to 1,000,000.
 
+last = 1000 # Change this to a smaller value for expedited testing
+for i in range(5,last+1,5):
+	print(i)
 
-
-# Counting, the Dojo Way
+print("Counting, the Dojo Way")
 # Print integers 1 to 100. If divisible by 5, print "Coding" instead. If by 10, also print " Dojo".
 
+for i in range(1,101):
+	if i%5==0:
+		if i%10==0:
+			print("Coding Dojo")
+		else:
+			print("Coding")
+	else:
+		print(i)
 
-
-# Whoa. That Sucker's Huge
+print("Whoa. That Sucker's Huge")
 # Add odd integers from 0 to 500,000, and print the final sum.
 
+sum = 0
+for i in range(1,500000,2):
+	sum+=i
+print(sum)
 
-
-# Countdown by Fours
+print("Countdown by Fours")
 # Print positive numbers starting at 2018, counting down by fours (exclude 0).
 
+for i in range(2018,0,-4):
+	print(i)
 
-
-# Flexible Countdown
+print("Flexible Countdown")
 # Based on earlier "Countdown by Fours", given lowNum, highNum, mult, print multiples of mult from lowNum to highNum, using a FOR loop.
 # For (2,9,3), print 3 6 9 (on successive lines)
 
+lowNum = 2
+highNum = 9
+mult = 3
 
+for i in range(lowNum, highNum+1):
+	if i%mult==0:
+		print(i)
 
-# Part 2
+print("Part 2")
 
 list1 = [3,5,1,2]
 for i in list1:
