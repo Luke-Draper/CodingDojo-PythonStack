0678fe02db1eb38fa55dce6171ea5a10df7087c7
Luke Draper <lukepdraper@gmail.com>
2018-10-07 22:13:20 -0500

    Functional remove

--- 3.Python/3.OOP/6.SLists/assignment.py
+++ 3.Python/3.OOP/6.SLists/assignment.py
@@ -24,12 +24,12 @@ class SList:
 			self.head = self.head.next
 		else:
 			current_node = self.head
-			while (current_node.next.next != None):
+			while (current_node.next != None):
 				if current_node.next.value == value:
-					current_node.next = current_node.next.next
+					current_node.next = current_node.next.next # This equals the next in line if the matched value is in the middle but it equals none if the matched value is the tail
 					break
 				current_node = current_node.next
-			
+		return self
 	
 	def printAllValues(self):
 		output = ""
@@ -41,8 +41,18 @@ class SList:
 		print(output)
 		return self
 
-list = SList(5)
-list.addNode(7)
-list.addNode(9)
-list.addNode(1)
-list.printAllValues()
+list1 = SList(5)
+list1.addNode(7)
+list1.addNode(9)
+list1.addNode(1)
+list1.printAllValues()
+
+
+list2 = SList(5)
+list2.addNode(7)
+list2.addNode(9)
+list2.addNode(1)
+list2.removeNode(9) # removes 9, which is one of the middle nodes in the list
+list2.removeNode(5) # removes 5, which is the first value in the list
+list2.removeNode(1) # removes 1, which is the last node in the list
+list2.printAllValues()
