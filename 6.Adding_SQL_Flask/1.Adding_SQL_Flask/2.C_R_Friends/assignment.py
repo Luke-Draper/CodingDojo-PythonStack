28a0a2c137321ad1b889ad7a19a8c81ba1910ebe
Luke Draper <lukepdraper@gmail.com>
2018-10-25 19:59:02 -0500

    C+R Friends Complete

---
+++ 6.Adding_SQL_Flask/1.Adding_SQL_Flask/2.C_R_Friends/assignment.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect, session
+import random
+from mysqlconnection import connectToMySQL
+app = Flask(__name__)
+app.secret_key = 'JimmyKnows'
+
+@app.route('/')
+def default():
+	return render_template("index.html", friends=get_friends())
+
+def get_friends():
+	mysql = connectToMySQL('c_r_friends')
+	return mysql.query_db("SELECT first_name, last_name, occupation FROM friends;")
+
+# SELECT first_name, last_name, occupation FROM friends;
+
+@app.route('/add_friend', methods=['POST'])
+def result():
+	if validate_friend(request.form):
+		add_friend(request.form)
+	return redirect('/')
+
+def validate_friend(friend_dict):
+	output = True
+	if len(friend_dict['first_name']) < 1:
+		output = False
+	if len(friend_dict['last_name']) < 1:
+		output = False
+	if len(friend_dict['occupation']) < 1:
+		output = False
+	return output
+
+def add_friend(friend_dict):
+	mysql = connectToMySQL('c_r_friends')
+	return mysql.query_db("INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES ('"+ friend_dict['first_name'] +"', '"+ friend_dict['last_name'] +"', '"+ friend_dict['occupation'] +"', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());")
+
+# INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at)
+# VALUES ("Zaphod", "Beeblebrox", "President of the Galaxy", CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+
+if __name__=="__main__":
+	app.run(debug=True)
